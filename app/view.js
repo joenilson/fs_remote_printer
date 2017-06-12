f1446bbd4451d1d3dbbe9436c06938eeaaf968f9
Joe Nilson <joenilson@gmail.com>
2017-06-11 22:46:46 -0400

    fs_remote_printer: generada lógica básica para buscar y seleccionar las impresoras y guardar la impresora a utilizar

--- app/view.js
+++ app/view.js
@@ -14,13 +14,65 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+//Cargamos jquery
 let $ = require('jquery');
+//Cargamos filesystem para leer el archivo de configuración de la impresora a utilizar
+let fs = require('fs');
+//Indicamos cual es el archivo de configuración de la impresora
+let configuracion = 'app/config.json';
+//Creamos la variable donde guardaremos la configuracion
+let config_data = false;
+//Leemos el listado de las impresoras de printer que la cargamos en impresoras.js
 var impresoras = printer.getPrinters();
-$.each(impresoras, function(index, element){
-    $('#lista_impresoras ul').append(
-        $('<li class="list-group-item">').append(
-            $('<a>').attr('href','/impresora/'+element.name).append(
-                $('<span>').attr('class', 'tab').append(element.name)
-    )));
-});
 
+
+function escribirConfiguracion(){
+    var json = JSON.stringify(config_data);
+    fs.writeFile(configuracion, json, (err) => {
+        if (err) {
+            $('#alerta_generica_texto').html();
+            $('#alerta_generica_texto').html('Hubo un error intentando crear el archivo de configuración, por favor cierre la aplicación y vuelva a abrirla.');
+            $('#alerta_generica').show();
+        }
+    });
+}
+function cargarConfiguracion(){
+    if(fs.existsSync(configuracion)){
+        try {
+            config_data = JSON.parse(fs.readFileSync(configuracion, 'utf8'));
+        }catch(e){
+            $('#alerta_configuracion').show();
+        }
+    }
+    else {
+        //Si no existe el archivo lo creamos con valores vacios
+        config_data = '{\n"nombre_impresora":"",\n"tipo_impresora":""\n}\n';
+        escribirConfiguracion();
+    }
+}
+
+function cargarImpresoras(){
+    $("#lista_impresoras ul").empty();
+    $("#lista_impresoras ul").append('<li class="list-group-item active">Impresoras disponibles</li>');
+    $.each(impresoras, function(index, element){
+        var badge = '<button class="btn btn-sm btn-link pull-right" onclick="elegirImpresora(\''+element.name+'\')"><i class="fa fa-star-o"></i></button>';
+        var success = '';
+        if(element.name === config_data.nombre_impresora){
+            badge = '<button class="btn btn-sm btn-link pull-right" disabled><i class="fa fa-star"></i></button>';
+            success = ' list-group-item-success';
+        }
+        $("#lista_impresoras ul").append('<li class="list-group-item'+success+'">'+element.name+' '+badge+'</li>');
+    });
+}
+
+function elegirImpresora(nombre){
+    if(nombre.length > 0){
+        config_data.nombre_impresora = nombre;
+        escribirConfiguracion();
+    }
+    cargarImpresoras();
+}
+//Cargamos la configuracion del archivo json si existe, si no existe lo creamos
+cargarConfiguracion();
+//Cargamos el listado de impresoras
+cargarImpresoras();
