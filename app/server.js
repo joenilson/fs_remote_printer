49cf9c65f8e0d82000b7cd42be2cca93b51488da
Joe Nilson <joenilson@gmail.com>
2017-08-07 18:03:33 -0400

    fs_remote_printer: mejoras en archivo server.js

--- app/server.js
+++ app/server.js
@@ -14,6 +14,9 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+/** global: config_data */
+/** global: notifier */
+/** global: path */
 var express = require('express');
 var app = express();
 
